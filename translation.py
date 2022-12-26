778f0e8f4c3c0f81a669086cbbb522516970de9b
Fadeev Egor <fadeevegor@yandex.ru>
2022-12-26 11:03:50 +0300

    Majorly refactored translation.py.

--- translation.py
+++ translation.py
@@ -1,62 +1,97 @@
 import random
 from operator import methodcaller, attrgetter
-from typing import Iterable
+from typing import Type
+from pathlib import Path
 
 from rich import print
-from prompt_toolkit import prompt
-from prompt_toolkit.document import Document
-from prompt_toolkit.completion import Completer, Completion, CompleteEvent
-from prompt_toolkit.validation import Validator, ValidationError
-from prompt_toolkit.auto_suggest import AutoSuggest, Suggestion
 import typer
 from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+from InquirerPy.validator import PathValidator
 
-from turkishinput import prompt_turkish
-from russianinput import prompt_russian
+from dictionaryitem import DictionaryItem
 from turkrutdictionaryitem import TurkrutDictionaryItem
 
 
-dictionary_item_types = {
-    "turkrut": TurkrutDictionaryItem
-}
-
-
-def prompt_target_language():
-    choice = inquirer.select(
-        message="Choose language you want to translate to.",
-        choices=["Turkish → Russian", "Russian → Turkish"],
+def prompt_filename():
+    return inquirer.filepath(
+        message="Enter file to practice: ",
+        default=str(Path.cwd()) + "\\",
+        validate=PathValidator(is_file=True),
+        only_files=True,
+        mandatory=True,
     ).execute()
-    return choice.lower().split(" → ")[-1]
 
 
-def practice_translation(
-        filename: str,
-        shuffle: bool = True,
-        type: str = "turkrut"
-):
-    dict_item = dictionary_item_types[type]
+def prompt_filetype() -> Type[DictionaryItem]:
+    return inquirer.select(
+        message="What is the format of the file?",
+        choices=[
+            Choice(value=TurkrutDictionaryItem, name="turkrut.ru"),
+        ],
+    ).execute()
+
+
+def prompt_target_language() -> str:
+    return inquirer.select(
+        message="Choose language you want to translate to.",
+        choices=[
+            Choice(value="russian", name="Turkish → Russian"),
+            Choice(value="turkish", name="Russian → Turkish")
+        ],
+    ).execute()
+
+
+def prompt_shuffle() -> bool:
+    return inquirer.select(
+        message="What order of questions would you prefer?",
+        choices=[
+            Choice(value=True, name="Shuffled order"),
+            Choice(value=False, name="As it is in the file")
+        ],
+    ).execute()
+
+
+def answer_with_prompt(word: DictionaryItem, target_language: str):
+    if target_language == "russian":
+        correct_translation = word.russian
+        response = word.ask_translation_to_russian()
+        is_correct = word.check_translation_to_russian(response)
+    else:
+        correct_translation = word.turkish
+        response = word.ask_translation_to_turkish()
+        is_correct = word.check_translation_to_turkish(response)
+    if is_correct:
+        print(
+            f"[green]Correct![/green] In the file: [green]{correct_translation}[/green]."
+        )
+    else:
+        print(
+            f"[red]Incorrect![/red] In the file: [green]{correct_translation}[/green]."
+        )
+    return is_correct, word
+
+
+def translation() -> None:
+    """
+    Practice translation of words from Turkish to Russian or vice versa.
+    """
+    dict_item = prompt_filetype()
+
+    filename = prompt_filename()
     dictionary = []
     with open(filename, encoding="utf-8") as f:
         for line in f:
             dictionary.append(dict_item(line))
+
+    shuffle = prompt_shuffle()
     if shuffle:
         random.shuffle(dictionary)
 
     target_language = prompt_target_language()
-    ask_translation = methodcaller(
-        f"ask_translation_to_{target_language}"
-    )
-    answer_getter = attrgetter(target_language)
     for word in dictionary:
-        is_correct = ask_translation(word)
-        if is_correct:
-            print(f"[green]Correct![/green]")
-        else:
-            correct_answer = answer_getter(word)
-            print(
-                f"[red]Incorrect![/red] Right answer: [green]{correct_answer}[/green]"
-            )
+        answer_with_prompt(word, target_language)
 
 
 if __name__ == "__main__":
-    typer.run(practice_translation)
+    typer.run(translation)
