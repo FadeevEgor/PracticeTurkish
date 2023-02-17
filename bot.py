939e71d2a147629bac93bfbd298abd41bb5db4a0
Fadeev Egor <fadeevegor@yandex.ru>
2023-02-17 11:17:47 +0300

    config validation and template

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from configparser import ConfigParser
+from configparser import ConfigParser, SectionProxy
 from dataclasses import dataclass
 
 import requests
@@ -21,13 +21,23 @@ class APIConfiguration:
     @classmethod
     def from_config(cls, path: str = "config.ini") -> "APIConfiguration":
         config = ConfigParser()
-        config.read(path)
-        API_section = config["BOT API"]
-        return cls(
-            API_section["URL"],
-            API_section["USER ID"],
-            API_section["TOKEN"]
-        )
+        if not config.read(path):
+            raise ConfigurationError(
+                f"Missing '{path}' configuration file!"
+            )
+
+        try:
+            API_section = config["BOT API"]
+        except KeyError:
+            raise ConfigurationError(
+                f"Your configuration file '{path}' is incorrect: missing 'BOT API' section."
+            )
+
+        URL = _get_and_check_str(API_section, "URL", path)
+        user_id = _get_and_check_int(API_section, "USER ID", path)
+        token = _get_and_check_str(API_section, "TOKEN", path)
+
+        return cls(URL, user_id, token)
 
 
 def send_to_telegram(
@@ -45,7 +55,33 @@ def send_to_telegram(
         }
     )
     if response.status_code == 403:
-        raise WrongToken("User token is wrong. Check your configuration file!")
+        raise WrongToken(
+            "User token is wrong. Please, ask telegram bot for correct one!"
+        )
     if response.status_code != 200:
         raise requests.HTTPError(response.text)
     return True
+
+
+def _get_and_check_int(section: SectionProxy, field: str, path: str) -> int:
+    try:
+        value = section.getint(field)
+    except ValueError:
+        raise ConfigurationError(
+            f"Your configuration file '{path}' is incorrect: '{field}' expected to be an integer."
+        )
+    if value is None:
+        raise ConfigurationError(
+            f"Your configuration file '{path}' is incorrect: missing '{field}' field."
+        )
+    return value
+
+
+def _get_and_check_str(section: SectionProxy, field: str, path: str) -> str:
+    try:
+        value = section[field]
+    except KeyError:
+        raise ConfigurationError(
+            f"Your configuration file {path} is incorrect: missing '{field}' field."
+        )
+    return value
